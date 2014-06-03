6bdebfcff8566d186f3fe8f781a03916fae4bb86
Andy Mai <andymai@rescomp-13-213410.stanford.edu>
2014-06-03 01:28:36 -0700

    debug

--- app.js
+++ app.js
@@ -83,10 +83,9 @@ app.post("/create_transaction", function (req, res) {
     if (result) {
       if (result.success) {
         var mailOptions = {
-          from: "ProudPay <proudpayreceipt@gmail.com>", // sender address
           to: req.body.customer_email, // list of receivers
           subject: "You paid " + req.body.merchant + " $" + req.body.amount, // Subject line
-          text: "Thanks for using ProudPay!", // plaintext body
+          text: "Thank you for using ProudPay!", // plaintext body
         }
 
         // send mail with defined transport object
