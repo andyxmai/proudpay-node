2876d292385371c505f977b3cfaac7460fbf6bd0
Andy Mai <andymai@rescomp-13-213410.stanford.edu>
2014-06-21 09:49:25 -0700

    factored out code

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var users = require('./routes/users');
 var about = require('./routes/about');
 var terms = require('./routes/terms');
 var privacy = require('./routes/privacy');
-//var customerInfo = require('./routes/getCustomerInfo');
+var createTransaction = require('./routes/createTransaction');
 
 var app = express();
 
@@ -57,6 +57,7 @@ app.use('/users', users);
 app.use('/about', about);
 app.use('/terms', terms);
 app.use('/privacy', privacy);
+app.use('/create_transaction', createTransaction);
 //app.use('/get_customer_info', customerInfo);
 
 app.get("/braintree", function (req, res) {
@@ -78,117 +79,47 @@ app.get("/get_customer_info", function(req, res) {
   });
 });
 
-function mailCustomerReceipt(req) {
-  var mailOptions = {
-    from: "ProudPay <proudpayreceipt@gmail.com>", // sender address
-    to: req.body.customer_email, // list of receivers
-    subject: "You paid " + req.body.merchant + " $" + req.body.customerFinalAmount, // Subject line
-    text: "Thank you for using ProudPay!", // plaintext body
-  }
+// function mailCustomerReceipt(req) {
+//   var mailOptions = {
+//     from: "ProudPay <proudpayreceipt@gmail.com>", // sender address
+//     to: req.body.customer_email, // list of receivers
+//     subject: "You paid " + req.body.merchant + " $" + req.body.customerFinalAmount, // Subject line
+//     text: "Thank you for using ProudPay!", // plaintext body
+//   }
 
-  // send mail with defined transport object
-  smtpTransport.sendMail(mailOptions, function(error, response){
-    if(error){
-        console.log(error);
-    }else{
-        console.log("Message sent: " + response.message);
-    }
-      // if you don't want to use this transport object anymore, uncomment following line
-      //smtpTransport.close(); // shut down the connection pool, no more messages
-  });   
-}
+//   // send mail with defined transport object
+//   smtpTransport.sendMail(mailOptions, function(error, response){
+//     if(error){
+//         console.log(error);
+//     }else{
+//         console.log("Message sent: " + response.message);
+//     }
+//       // if you don't want to use this transport object anymore, uncomment following line
+//       //smtpTransport.close(); // shut down the connection pool, no more messages
+//   });   
+// }
 
-function cashback() {
-  var Cashback = Parse.Object.extend("Cashback");
-  var query = new Parse.Query(Cashback);
-  query.equalTo("user", {
-    __type: "Pointer",
-    className: "_User",
-    objectId: req.body.customer_id
-  });
+// app.post("/create_transaction", function (req, res) {
+//   //Parse.Cloud.useMasterKey();
+//   var saleRequest = {
+//     customerId: req.body.customer_id,
+//     amount: req.body.total,
+//     options: {
+//       submitForSettlement: true
+//     }
+//   };
 
-  query.first({
-    success: function(cashback) {
-      var cashBackFloat = parseFloat(cashback.get("cashbackCount"));
-      var totalFloat = parseFloat(req.body.customerFinalAmount) + cashBackFloat;
-      var currCreditFloat = parseFloat(customer.get("credits"));
-      if (totalFloat > 100.00) {
-
-          var additionalCreditsFloat = Math.floor(totalFloat/100);
-          var newCredits = currCreditFloat+additionalCreditsFloat.toFixed(2).toString();
-          customer.set("credits", newCredits);
-          var newCashBackCount = (totalFloat-(additionalCreditsFloat*100)).toFixed(2).toString();
-          console.log("greater than 100; cashBackCount: "+newCashBackCount);
-          cashback.set("cashbackCount", newCashBackCount);
-          cashback.save();
-          //object[@"cashBackCount"] = [NSString stringWithFormat:@"%0.2f", totalFloat-(additionalCredits*100)];
-      } else {
-        var newCashBackCount = (cashBackFloat+parseFloat(req.body.customerFinalAmount)).toFixed(2).toString();
-        console.log("less than 100; cashBackCount: "+newCashBackCount);
-        customer.set("cashbackCount", newCashBackCount);
-        customer.save();
-          //object[@"cashBackCount"] = [NSString stringWithFormat:@"%0.2f", cashBackFloat + amountFloat];
-      }
-    },
-    error: function() {
-    }
-  });
-
-
-  query.get(req.body.customer_id, {
-    success: function(customer) {
-      console.log("got customer");
-      // The object was retrieved successfully.
-      var cashBackFloat = parseFloat(customer.get("cashBackCount"));
-      var totalFloat = parseFloat(req.body.customerFinalAmount) + cashBackFloat;
-      var currCreditFloat = parseFloat(customer.get("credits"));
-      if (totalFloat > 100.00) {
-
-          var additionalCreditsFloat = Math.floor(totalFloat/100);
-          var newCredits = currCreditFloat+additionalCreditsFloat.toFixed(2).toString();
-          customer.set("credits", newCredits);
-          var newCashBackCount = (totalFloat-(additionalCreditsFloat*100)).toFixed(2).toString();
-          console.log("greater than 100; cashBackCount: "+newCashBackCount);
-          customer.set("cashBackCount", newCashBackCount);
-          customer.save();
-          //object[@"cashBackCount"] = [NSString stringWithFormat:@"%0.2f", totalFloat-(additionalCredits*100)];
-      } else {
-        var newCashBackCount = (cashBackFloat+parseFloat(req.body.customerFinalAmount)).toFixed(2).toString();
-        console.log("less than 100; cashBackCount: "+newCashBackCount);
-        customer.set("cashBackCount", newCashBackCount);
-        customer.save();
-          //object[@"cashBackCount"] = [NSString stringWithFormat:@"%0.2f", cashBackFloat + amountFloat];
-      }
-    },
-    error: function(object, error) {
-      // The object was not retrieved successfully.
-      // error is a Parse.Error with an error code and description.
-      console.log(error);
-    }
-  });
-}
-
-app.post("/create_transaction", function (req, res) {
-  //Parse.Cloud.useMasterKey();
-  var saleRequest = {
-    customerId: req.body.customer_id,
-    amount: req.body.total,
-    options: {
-      submitForSettlement: true
-    }
-  };
-
-  gateway.transaction.sale(saleRequest, function (err, result) {
-    //console.log(result);
-    res.send(result);
-    if (result) {
-      if (result.success) {
-        console.log("success");
-        mailCustomerReceipt(req);
-      }
-    }
-  });
-});
+//   gateway.transaction.sale(saleRequest, function (err, result) {
+//     //console.log(result);
+//     res.send(result);
+//     if (result) {
+//       if (result.success) {
+//         console.log("success");
+//         mailCustomerReceipt(req);
+//       }
+//     }
+//   });
+// });
 
 app.post("/create_customer", function (req, res) {
   var customerRequest = {
--- routes/createTransaction.js
+++ routes/createTransaction.js
@@ -11,7 +11,7 @@ var gateway = braintree.connect({
     privateKey:   'cb4ee99fabce5fdcf88926d7a476ceae'
 });
 
-router.post('/create_transaction', function(req, res) {
+router.post('/', function(req, res) {
   var saleRequest = {
     customerId: req.body.customer_id,
     amount: req.body.total,
@@ -21,30 +21,104 @@ router.post('/create_transaction', function(req, res) {
   };
 
   gateway.transaction.sale(saleRequest, function (err, result) {
-    console.log(result);
     res.send(result);
     if (result) {
       if (result.success) {
-        var mailOptions = {
-          from: "ProudPay <proudpayreceipt@gmail.com>", // sender address
-          to: req.body.customer_email, // list of receivers
-          subject: "You paid " + req.body.merchant + " $" + req.body.customerFinalAmount, // Subject line
-          text: "Thank you for using ProudPay!", // plaintext body
-        }
-
-        // send mail with defined transport object
-        smtpTransport.sendMail(mailOptions, function(error, response){
-          if(error){
-              console.log(error);
-          }else{
-              console.log("Message sent: " + response.message);
-          }
-            // if you don't want to use this transport object anymore, uncomment following line
-            //smtpTransport.close(); // shut down the connection pool, no more messages
-        });   
+        console.log("success");
+        mailCustomerReceipt(req);
       }
     }
   });
 });
+
+function mailCustomerReceipt(req) {
+  var mailOptions = {
+    from: "ProudPay <proudpayreceipt@gmail.com>", // sender address
+    to: req.body.customer_email, // list of receivers
+    subject: "You paid " + req.body.merchant + " $" + req.body.customerFinalAmount, // Subject line
+    text: "Thank you for using ProudPay!", // plaintext body
+  }
+
+  // send mail with defined transport object
+  smtpTransport.sendMail(mailOptions, function(error, response){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Message sent: " + response.message);
+    }
+      // if you don't want to use this transport object anymore, uncomment following line
+      //smtpTransport.close(); // shut down the connection pool, no more messages
+  });   
+}
+
+function cashback() {
+  var Cashback = Parse.Object.extend("Cashback");
+  var query = new Parse.Query(Cashback);
+  query.equalTo("user", {
+    __type: "Pointer",
+    className: "_User",
+    objectId: req.body.customer_id
+  });
+
+  query.first({
+    success: function(cashback) {
+      var cashBackFloat = parseFloat(cashback.get("cashbackCount"));
+      var totalFloat = parseFloat(req.body.customerFinalAmount) + cashBackFloat;
+      var currCreditFloat = parseFloat(customer.get("credits"));
+      if (totalFloat > 100.00) {
+
+          var additionalCreditsFloat = Math.floor(totalFloat/100);
+          var newCredits = currCreditFloat+additionalCreditsFloat.toFixed(2).toString();
+          customer.set("credits", newCredits);
+          var newCashBackCount = (totalFloat-(additionalCreditsFloat*100)).toFixed(2).toString();
+          console.log("greater than 100; cashBackCount: "+newCashBackCount);
+          cashback.set("cashbackCount", newCashBackCount);
+          cashback.save();
+          //object[@"cashBackCount"] = [NSString stringWithFormat:@"%0.2f", totalFloat-(additionalCredits*100)];
+      } else {
+        var newCashBackCount = (cashBackFloat+parseFloat(req.body.customerFinalAmount)).toFixed(2).toString();
+        console.log("less than 100; cashBackCount: "+newCashBackCount);
+        customer.set("cashbackCount", newCashBackCount);
+        customer.save();
+          //object[@"cashBackCount"] = [NSString stringWithFormat:@"%0.2f", cashBackFloat + amountFloat];
+      }
+    },
+    error: function() {
+    }
+  });
+
+
+  query.get(req.body.customer_id, {
+    success: function(customer) {
+      console.log("got customer");
+      // The object was retrieved successfully.
+      var cashBackFloat = parseFloat(customer.get("cashBackCount"));
+      var totalFloat = parseFloat(req.body.customerFinalAmount) + cashBackFloat;
+      var currCreditFloat = parseFloat(customer.get("credits"));
+      if (totalFloat > 100.00) {
+
+          var additionalCreditsFloat = Math.floor(totalFloat/100);
+          var newCredits = currCreditFloat+additionalCreditsFloat.toFixed(2).toString();
+          customer.set("credits", newCredits);
+          var newCashBackCount = (totalFloat-(additionalCreditsFloat*100)).toFixed(2).toString();
+          console.log("greater than 100; cashBackCount: "+newCashBackCount);
+          customer.set("cashBackCount", newCashBackCount);
+          customer.save();
+          //object[@"cashBackCount"] = [NSString stringWithFormat:@"%0.2f", totalFloat-(additionalCredits*100)];
+      } else {
+        var newCashBackCount = (cashBackFloat+parseFloat(req.body.customerFinalAmount)).toFixed(2).toString();
+        console.log("less than 100; cashBackCount: "+newCashBackCount);
+        customer.set("cashBackCount", newCashBackCount);
+        customer.save();
+          //object[@"cashBackCount"] = [NSString stringWithFormat:@"%0.2f", cashBackFloat + amountFloat];
+      }
+    },
+    error: function(object, error) {
+      // The object was not retrieved successfully.
+      // error is a Parse.Error with an error code and description.
+      console.log(error);
+    }
+  });
+}
 
 module.exports = router;
